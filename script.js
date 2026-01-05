b91ef9fd8b9e27bc96d0b76dfefa183c4c23376c
Bharath552-bit <patthembharath32@gmail.com>
2026-01-05 20:14:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+document.getElementById('openModal').onclick = function() {
+    document.getElementById('modal').style.display = "block";
+}
+
+document.querySelector('.close-modal').onclick = function() {
+    document.getElementById('modal').style.display = "none";
+}
+
+// Close the modal if the user clicks anywhere outside of it
+window.onclick = function(event) {
+    const modal = document.getElementById('modal');
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
